9e2ba5c437ec9564c9c91a2a437f56cf2f80d0cc
chawanakorns <chawanakorn_s@cmu.ac.th>
2024-10-09 17:16:00 +0700

    Lab 11 (3.2)

--- src/main/java/se331/lab/rest/controller/OrganizerController.java
+++ src/main/java/se331/lab/rest/controller/OrganizerController.java
@@ -3,7 +3,9 @@ package se331.lab.rest.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import se331.lab.rest.entity.OrganizerDTO;
 import se331.lab.rest.service.OrganizerService;
 import se331.lab.rest.util.LabMapper;
 
@@ -11,8 +13,16 @@ import se331.lab.rest.util.LabMapper;
 @RequiredArgsConstructor
 public class OrganizerController {
     final OrganizerService organizerService;
+
     @GetMapping("/organizers")
     ResponseEntity<?> getOrganizers() {
         return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(organizerService.getAllOrganizer()));
+    }
+
+    @GetMapping("/organizers/{id}")
+    ResponseEntity<OrganizerDTO> getOrganizerById(@PathVariable Long id) {
+        return organizerService.getOrganizerById(id)
+                .map(organizer -> ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(organizer)))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
